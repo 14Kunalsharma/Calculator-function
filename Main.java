0fd44cb7edd86ae5638d22b7e68b6c8183c6d5f6
14Kunalsharma <166681796+14Kunalsharma@users.noreply.github.com>
2025-09-06 21:55:41 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,9 +13,9 @@ class Main {
 		}
     public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.print("Enter value of x: 		");
+		System.out.print("Enter value of x: ");
 		int x=sc.nextInt();
-		System.out.print("Enter value of y: 		");
+		System.out.print("Enter value of y: ");
 		int y=sc.nextInt();
 		cal(x,y);
     }
